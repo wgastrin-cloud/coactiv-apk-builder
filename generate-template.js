9c95449969b1eebeb6cbb0abe7e5f16c830efdae
wgastrin-cloud <w.gastrin@gmail.com>
2026-02-22 20:50:16 +0100

    Update generate-template.js

--- generate-template.js
+++ generate-template.js
@@ -112,7 +112,8 @@ dependencies {
 
   // AndroidManifest.xml
   fs.writeFileSync(path.join(ANDROID_DIR, 'app/src/main/AndroidManifest.xml'), `<?xml version="1.0" encoding="utf-8"?>
-<manifest xmlns:android="http://schemas.android.com/apk/res/android">
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="PLACEHOLDER_PACKAGE_ID">
     
     <uses-permission android:name="android.permission.INTERNET" />
     <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
